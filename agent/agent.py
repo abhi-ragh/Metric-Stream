0f9ada5bf1654a8bf8bbd2bea6493616c8cea315
Abhiragh A R <abhiragh0@gmail.com>
2025-11-20 16:56:14 +0530

    API + Agent Workflow Achieved

--- agent/agent.py
+++ agent/agent.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import psutil
 import time
+import requests
 
 date = datetime.today().strftime("%d-%m-%Y")
 
@@ -9,16 +10,28 @@ prev_bytes_sent = psutil.net_io_counters().bytes_sent
 
 while True:
     cpu_usage = psutil.cpu_percent()
-    mem_usage = psutil.virtual_memory()
-    disk_usage = psutil.disk_usage('/')
+    mem_usage = psutil.virtual_memory().percent
+    disk_usage = psutil.disk_usage('/').percent
+
     curr_bytes_recv = psutil.net_io_counters().bytes_recv
     curr_bytes_send = psutil.net_io_counters().bytes_sent
-    print("!!!!!!!!!!!!!!!!!!!!!!!!")
-    print("CPU: ",cpu_usage)
-    print("Memory: ",mem_usage.percent)
-    print("Disk: ", disk_usage.percent)
-    print("Bytes Sent: ",round((curr_bytes_send-prev_bytes_sent)/1024,2))
-    print("Bytes Received: ",round((curr_bytes_recv-prev_bytes_recv)/1024,2))
+    
+    bytes_send = (curr_bytes_send-prev_bytes_sent)/1024
+    bytes_recv = (curr_bytes_recv-prev_bytes_recv)/1024
+    
     prev_bytes_recv = curr_bytes_recv
     prev_bytes_sent = curr_bytes_send
+    
+    payload = {
+        "date": date, 
+        "cpu_usage": cpu_usage, 
+        "mem_usage": mem_usage, 
+        "disk_usage": disk_usage,
+        "bytes_recv": bytes_recv,
+        "bytes_send": bytes_send
+    }
+
+    response = requests.post('http://127.0.0.1:8000/metrics',json=payload)
+    print(response)
+
     time.sleep(1)
